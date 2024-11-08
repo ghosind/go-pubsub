432a89b3a8a6779af3a2831f5061095d10cc4d5e
ghosind <ghosind@gmail.com>
2024-11-08 23:13:33 +0800

    feat: add ContentType and MessageID fields to the PublishInput
    Signed-off-by: ghosind <ghosind@gmail.com>

--- publish.go
+++ publish.go
@@ -5,6 +5,10 @@ type PublishInput struct {
 	Queue string
 	// Body is the message body.
 	Body []byte
+	// ContentType is the content type of the message.
+	ContentType string
+	// MessageID is the id of the message.
+	MessageID string
 	// Persistent is a flag that indicates whether the message should be persisted.
 	Persistent bool
 }
@@ -18,6 +22,18 @@ func (input *PublishInput) SetQueue(queue string) *PublishInput {
 // SetBody sets the message body.
 func (input *PublishInput) SetBody(body []byte) *PublishInput {
 	input.Body = body
+	return input
+}
+
+// SetContentType sets the content type of the message.
+func (input *PublishInput) SetContentType(contentType string) *PublishInput {
+	input.ContentType = contentType
+	return input
+}
+
+// SetMessageID sets the id of the message.
+func (input *PublishInput) SetMessageID(messageId string) *PublishInput {
+	input.MessageID = messageId
 	return input
 }
 
